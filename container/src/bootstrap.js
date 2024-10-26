c2627eca3480182a0a997a3b06ff54472aba324c
Gabriel Diaz <diazhernandezgabriel2001@gmail.com>
2024-10-26 19:29:28 -0300

    feat: consuming remote modules

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,4 +1,7 @@
-import 'products/ProductsIndex';
-import 'cart/CartShow';
+import { mount as productsMount } from 'products/ProductsIndex';
+import { mount as cartMount } from 'cart/CartIndex';
 
 console.log('container');
+
+productsMount(document.querySelector('#container-products'));
+cartMount(document.querySelector('#container-cart'));
